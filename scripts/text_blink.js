f54dabd7da00b81a5f5252443438d813a07c3110
JasonMilhaven <JasonMilhaven@users.noreply.github.com>
2020-12-06 20:38:18 -0500

    updated banner list margin-top

--- scripts/text_blink.js
+++ scripts/text_blink.js
@@ -1,7 +1,4 @@
-
-
 const PAGE_SCROLL_Y_THRESHOLD = 50;
-const SCROLL_MOD = 40; //20.0; // how fast the scroll links go
 
 var banner = document.getElementById("banner");
 var bannerDummy = document.getElementById("banner_dummy");
@@ -54,7 +51,7 @@ window.addEventListener("scroll", function(e)
 )
 
 
-const CHAR_DELAY = 60;
+const CHAR_DELAY = 70;
 var short1 = document.getElementById("short1");
 var msg = short1.innerText;
 short1.innerText = "";
@@ -84,36 +81,8 @@ function displayScreenText() {
 	}
 }
 
-var currentScroll;
-function smooth_scroll(to, current)
-{
-	current = current || window.pageYOffset;
-	
-	v = SCROLL_MOD * clamp01(to - current)
-	newY = current + v;
-	window.scrollTo(0, newY);
-	
-	if (Math.abs(current - to) > SCROLL_MOD && newY - window.pageYOffset <= SCROLL_MOD * 2)
-	{
-		clearTimeout(currentScroll);
-		currentScroll = setTimeout(
-			function()
-			{
-				smooth_scroll(to, newY)
-			},
-			SCROLL_MOD
-		)
-	}
-}
-
 function init() {
 	window.setTimeout(displayScreenText, 500);
-	
-	/*var linkHome = document.getElementById("link_home");
-	var linkServices = document.getElementById("link_services");
-	
-	linkHome.href = "javascript:smooth_scroll(window.innerHeight * (105/100));";
-	linkServices.href = "javascript:smooth_scroll(window.innerHeight * (215/100));";*/
 }
 
 function addBlinker() {
@@ -126,4 +95,3 @@ function addBlinker() {
 		}
 	}, 500)
 }
-
